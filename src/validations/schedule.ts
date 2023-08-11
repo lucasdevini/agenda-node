a4f713554b239188275ad5b97dbe447af81804c5
Lucas <lucvini56@gmail.com>
2023-08-11 10:38:35 -0300

    :bug: fix: Corrigindo erro de validação dos dados do formulário de agendamento

--- src/validations/schedule.ts
+++ src/validations/schedule.ts
@@ -2,17 +2,10 @@ import { body } from "express-validator";
 
 export const scheduleValidation = [
     body("date").notEmpty().withMessage("Data obrigatória!")
-    .isDate().withMessage("Data inválida!")
-    .trim(),
+      .isISO8601().withMessage("Data inválida!")
+      .trim(),
     body("hour")
-        .notEmpty().withMessage("Hora obrigatória!")
-        .custom((value) => {
-            // Verifica se a hora está no formato "HH:mm"
-            const timePattern = /^([01]\d|2[0-3]):[0-5]\d$/;
-            if (!timePattern.test(value)) {
-              throw new Error("Hora inválida (formato esperado: HH:mm)");
-            }
-            return true;
-          })
-        .trim()
+      .notEmpty().withMessage("Hora obrigatória!")
+      .matches(/^([01]\d|2[0-3]):[0-5]\d$/, "i").withMessage("Hora inválida (formato esperado: HH:mm)")
+      .trim()
 ]
