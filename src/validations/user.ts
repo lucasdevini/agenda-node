9eb533d1236348b21dda4fa88c691022183403fb
Lucas <lucvini56@gmail.com>
2023-08-16 07:50:38 -0300

    :boom: fix: Alteração na lógica de mensagens da validação de usuários

--- src/validations/user.ts
+++ src/validations/user.ts
@@ -1,9 +1,14 @@
 import { body } from "express-validator";
 
 export const userValidation = [
-    body("email").notEmpty().withMessage("Email obrigatório!").isEmail().trim(),
+    body("email")
+    .trim()
+    .notEmpty().withMessage("Email obrigatório!")
+    .if(body("email").notEmpty())
+    .isEmail().withMessage("Formato de email não reconhecido"),
     body("password")
         .notEmpty().withMessage("Senha obrigatória!")
+        .if(body("password").notEmpty())
         .isLength({min: 8, max: 16}).withMessage("A senha deve conter entre 8 e 16 caracteres")   
-        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/).withMessage('A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial.'),
+        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/).withMessage('A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial.'),
 ]
