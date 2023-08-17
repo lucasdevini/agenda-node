e5564435ebf62bec8bb07b00c2064569448686b9
Lucas <lucvini56@gmail.com>
2023-08-17 14:51:21 -0300

    :sparkles: feat: adicionando novos campos ao model de usuário

--- src/models/user.ts
+++ src/models/user.ts
@@ -3,7 +3,10 @@ import { sequelize } from '../instances/sql';
 
 export interface UserInstance extends Model {
     id: number,
+    name: string,
+    date: Date,
     email: string,
+    phone: string,
     password: string,
     role: string
 }
@@ -15,9 +18,18 @@ export const User = sequelize.define<UserInstance>('User',
         autoIncrement: true,
         type: DataTypes.INTEGER
     },
+    name: {
+        type: DataTypes.STRING,
+    },
+    date: {
+        type: DataTypes.DATE
+    },
     email: {
         type: DataTypes.STRING,
         unique: true,
+    },
+    phone: {
+        type: DataTypes.STRING
     },
     password: {
         type: DataTypes.STRING
