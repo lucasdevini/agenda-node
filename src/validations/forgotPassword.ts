a09f904328ed0abe71eaa2be28b0b698f77f5b19
Lucas <lucvini56@gmail.com>
2023-08-21 12:09:28 -0300

    :sparkles: feat: validações dos campos da página 'esqueci minha senha'

---
+++ src/validations/forgotPassword.ts
@@ -0,0 +1,27 @@
+import { body } from "express-validator";
+
+export const forgotPasswordValidation = [
+    body("date")
+        .notEmpty()
+        .if(body("date").notEmpty())
+        .isISO8601().withMessage('A data de nascimento deve ser uma data válida.')
+        .custom((value) => {
+            const currentDate = new Date();
+            const userDate = new Date(value);
+            const ageDiff = currentDate.getFullYear() - userDate.getFullYear();
+            if (ageDiff < 18) {
+              throw new Error('Você deve ter pelo menos 18 anos para se registrar.');
+            }
+            return true;
+          }),
+    body("email")
+        .trim()
+        .notEmpty()
+        .if(body("email").notEmpty())
+        .isEmail().withMessage("Formato de email não reconhecido"),
+    body("phone")
+        .trim()
+        .notEmpty()
+        .if(body("phone").notEmpty())
+        .matches(/^\(\d{2}\) \d{4,5}-\d{4}$/).withMessage('O número de telefone deve estar no formato (xx) xxxx-xxxx.')
+]
