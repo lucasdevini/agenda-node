2124e18d0c291e5b48254f816734896d9bd0ff9d
Lucas <lucvini56@gmail.com>
2023-08-22 16:26:17 -0300

    :sparkles: feat: adicionando validações para campos de pergunta e resposta

--- src/validations/userSignUp.ts
+++ src/validations/userSignUp.ts
@@ -3,12 +3,12 @@ import { body } from "express-validator";
 export const userSignUpValidation = [
     body("name")
         .trim()
-        .notEmpty().withMessage("Nome obrigatório!")
+        .notEmpty()
         .if(body("name").notEmpty())
         .isLength({ min: 3, max: 50 }).withMessage('O nome do usuário deve ter entre 3 e 50 caracteres.')
-        .matches(/^[a-zA-ZÀ-ÿ\s-]+$/).withMessage('O nome do usuário deve conter apenas letras, espaços e hífens.'),
+        .matches(/^[a-zA-ZÀ-ÿ\s-]+$/).withMessage('O nome do usuário deve conter apenas letras, espaços e/ou hífens.'),
     body("date")
-        .notEmpty().withMessage('A data de nascimento é obrigatória.')
+        .notEmpty()
         .if(body("date").notEmpty())
         .isISO8601().withMessage('A data de nascimento deve ser uma data válida.')
         .custom((value) => {
@@ -22,17 +22,29 @@ export const userSignUpValidation = [
           }),
     body("email")
         .trim()
-        .notEmpty().withMessage("Email obrigatório!")
+        .notEmpty()
         .if(body("email").notEmpty())
         .isEmail().withMessage("Formato de email não reconhecido"),
     body("phone")
         .trim()
-        .notEmpty().withMessage('O número de telefone é obrigatório.')
+        .notEmpty()
         .if(body("phone").notEmpty())
         .matches(/^\(\d{2}\) \d{4,5}-\d{4}$/).withMessage('O número de telefone deve estar no formato (xx) xxxx-xxxx.'),
     body("password")
-        .notEmpty().withMessage("Senha obrigatória!")
+        .notEmpty()
         .if(body("password").notEmpty())
         .isLength({min: 8, max: 16}).withMessage("A senha deve conter entre 8 e 16 caracteres")   
         .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/).withMessage('A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial.'),
+    body("answer1")
+        .trim()
+        .notEmpty()
+        .if(body("answer1").notEmpty())
+        .isLength({ max: 100 }).withMessage('O nome do usuário deve ter entre 3 e 100 caracteres.')
+        .matches(/^[a-zA-ZÀ-ÿ\s-]+$/).withMessage('A resposta deve conter apenas letras, espaços e/ou hífens.'),
+    body("answer2")
+        .trim()
+        .notEmpty()
+        .if(body("answer2").notEmpty())
+        .isLength({ max: 100 }).withMessage('O nome do usuário deve ter entre 3 e 100 caracteres.')
+        .matches(/^[a-zA-ZÀ-ÿ\s-]+$/).withMessage('A resposta deve conter apenas letras, espaços e/ou hífens.'),
 ]
