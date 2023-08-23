d180011ee6fae39db5e8646df4a801af846f95e4
Lucas <lucvini56@gmail.com>
2023-08-23 09:41:17 -0300

    :sparkles: feat: arquivo de validações para a rota reset-password

---
+++ src/validations/resetPassword.ts
@@ -0,0 +1,14 @@
+import { body } from "express-validator";
+
+export const resetPasswordValidation = [
+    body("newPassword")
+        .notEmpty()
+        .if(body("newPassword").notEmpty())
+        .isLength({min: 8, max: 16}).withMessage("A senha deve conter entre 8 e 16 caracteres")   
+        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/).withMessage('A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial.'),
+    body("confirmPassword")
+        .notEmpty()
+        .if(body("confirmPassword").notEmpty())
+        .isLength({min: 8, max: 16}).withMessage("A senha deve conter entre 8 e 16 caracteres")   
+        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/).withMessage('A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial.')
+]
